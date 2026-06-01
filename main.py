106d08c05ded3e28e5b9f3cf8b41f8b804a1f5f1
WojciechIBaranowski <194296@student.ue.wroc.pl>
2026-06-01 14:18:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -66,11 +66,11 @@ def calfp(frates, fac_rate):
 # Główna funkcja sterująca (odpowiednik int main())
 if __name__ == "__main__":
     frates = [
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 3, 0],
-        [0, 1, 0],
-        [0, 3, 0]
+        [1, 4, 6],
+        [0, 0, 6],
+        [3, 1, 0],
+        [4, 2, 2],
+        [1, 0, 0]
     ]
     
     fac_rate = 2
